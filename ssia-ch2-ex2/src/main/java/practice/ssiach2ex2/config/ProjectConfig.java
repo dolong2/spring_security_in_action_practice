5698f9db0394b690e25586c261e0cec7e3865cb8
dolong2 <k01066624566@gmail.com>
2023-12-28 13:40:06 +0900

    [add] 기본 유저 추가

--- ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package practice.ssiach2ex2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -10,6 +12,11 @@ public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         var userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("dolong")
+                .password("1234")
+                .authorities("read")
+                .build();
+        userDetailsManager.createUser(user);
         return userDetailsManager;
     }
 
