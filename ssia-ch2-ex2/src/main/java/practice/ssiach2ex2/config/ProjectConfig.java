4dc65261ae4c098172fe2fbaaca5e75f674cf6e7
dolong2 <k01066624566@gmail.com>
2023-12-30 13:42:50 +0900

    [add] authorizeHttpRequests 추가

--- ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/ProjectConfig.java
@@ -2,12 +2,14 @@ package practice.ssiach2ex2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -25,5 +27,14 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests.anyRequest().permitAll()
+        );
+
+        return http.build();
     }
 }
