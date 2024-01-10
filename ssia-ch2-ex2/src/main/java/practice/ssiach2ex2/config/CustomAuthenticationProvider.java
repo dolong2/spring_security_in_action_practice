26bcf3cde6a2eeef7b0aac15261ffee24a8fee1f
dolong2 <k01066624566@gmail.com>
2024-01-10 11:55:51 +0900

    [add] authenticate메서드 구현 추가

--- ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/CustomAuthenticationProvider.java
+++ ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/CustomAuthenticationProvider.java
@@ -1,16 +1,25 @@
 package practice.ssiach2ex2.config;
 
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         //인증 논리를 추가할 위치
-        return null;
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+        if ("dolong".equals(username) && "1234".equals(password))
+            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        else
+            throw new AuthenticationCredentialsNotFoundException("Error in authentication");
     }
 
     @Override
