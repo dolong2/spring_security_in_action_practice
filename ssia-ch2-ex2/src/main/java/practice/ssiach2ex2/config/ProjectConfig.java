caeb16c0dced048ae7daddfff363762f27b4b8eb
dolong2 <k01066624566@gmail.com>
2024-02-19 14:35:29 +0900

    [add] UserManagementConfig로 UserDetailsService, PasswordEncoder를 등록시키는 부분 추가

--- ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/practice/ssiach2ex2/config/ProjectConfig.java
@@ -3,12 +3,6 @@ package practice.ssiach2ex2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,25 +10,9 @@ public class ProjectConfig {
     private final CustomAuthenticationProvider authenticationProvider;
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsManager = new InMemoryUserDetailsManager();
-        UserDetails user = User.withUsername("dolong")
-                .password("1234")
-                .authorities("read")
-                .build();
-        userDetailsManager.createUser(user);
-        return userDetailsManager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests ->
-            authorizeRequests.anyRequest().permitAll()
+            authorizeRequests.anyRequest().authenticated()
         );
         http.authenticationProvider(authenticationProvider);
 
